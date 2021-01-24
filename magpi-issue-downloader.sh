1e77b35c58b93b3cd1180a558908cae956845088
Anton Grouchtchak <goldtom1423@gmail.com>
2021-01-24 17:41:27 +0100

    Add magpi-issue-downloader.sh

---
+++ magpi-issue-downloader.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+OUTDIR="issues"
+BASEURL="https://magpi.raspberrypi.org/"
+LATEST=101
+
+if [ ! -d "$OUTDIR" ]; then
+  mkdir "$OUTDIR"
+fi
+
+clear
+
+for i in $( seq 1 $LATEST )
+do
+  URL=$(curl -s "${BASEURL}issues/${i}/pdf/download/" | grep "c-link" | grep -o -P '(?<=href="/).*(?=">)')
+  wget -nc $BASEURL$URL -P $OUTDIR -q --show-progress --progress=bar:force:noscroll
+  clear
+done
+
+exit 0
